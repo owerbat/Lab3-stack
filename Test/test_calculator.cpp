3a47ebe3212ad0e43f763daa35ab0eaa10d5b021
owerbat <niktimak@gmail.com>
2017-11-15 09:10:26 +0300

    Test throw_when_try_topostfix_incorrect_string was added

--- Test/test_calculator.cpp
+++ Test/test_calculator.cpp
@@ -8,20 +8,30 @@ TEST(TCalculator, can_create_object) {
 
 TEST(TCalculator, false_check_when_opening_brackets_more_then_closing_breackets) {
 	TCalculator a("((2+2*3)");
+
 	EXPECT_EQ(0, a.check());
 }
 
 TEST(TCalculator, false_check_when_closing_brackets_more_then_opening_breackets) {
 	TCalculator a("(2+2)*3)");
+
 	EXPECT_EQ(0, a.check());
 }
 
 TEST(TCalculator, false_check_when_the_first_symbol_is_closing_breacket) {
 	TCalculator a(")((2+2*3)");
+
 	EXPECT_EQ(0, a.check());
 }
 
 TEST(TCalculator, false_check_when_the_last_symbol_is_opening_breacket) {
 	TCalculator a("(2+2*3))(");
+
 	EXPECT_EQ(0, a.check());
+}
+
+TEST(TCalculator, throw_when_try_topostfix_incorrect_string) {
+	TCalculator a(")2+3)(");
+
+	ASSERT_ANY_THROW(a.topostfix());
 }
