b99aa8255e4ffb2901166d9f898b2ca5b49856d0
owerbat <niktimak@gmail.com>
2017-11-15 09:35:45 +0300

    A few tests were added in stack

--- Test/test_stack.cpp
+++ Test/test_stack.cpp
@@ -21,72 +21,96 @@ TEST(TStack, copied_stack_is_equal_to_source_one)
 {
 	int i;
 	TStack<int> a(3), b;
+
+
 	for (i = 0; i < 3; i++) {
 		a.push(i);
 	}
 	b = a;
+
+
 	EXPECT_EQ(a, b);
 }
 
 TEST(TStack, copied_stack_has_its_own_memory)
 {
 	TStack<int> a(3), b;
+
+
 	for (int i = 0; i < a.getmaxsize(); i++) {
 		a.push(i);
 	}
 	b = a;
 	int tmp = b.pop();
 	b.push(5);
+
+
 	EXPECT_NE(a, b);
 }
 
 TEST(TStack, can_get_size){
 	TStack<int> st(5);
+
+
 	for (int i = 0; i < 3; i++) {
 		st.push(i);
 	}
+
+
 	EXPECT_EQ(3, st.getsize());
 }
 
 TEST(TStack, can_get_maxsize){
 	TStack<int> st(5);
+
 	EXPECT_EQ(5, st.getmaxsize());
 }
 
 TEST(TStack, throws_when_get_top_in_empty_stack){
 	TStack<int> st(3);
+
 	ASSERT_ANY_THROW(st.top());
 }
 
 TEST(TStack, throws_when_get_pop_in_empty_stack){
 	TStack<int> st(3);
+
 	ASSERT_ANY_THROW(st.pop());
 }
 
 TEST(TStack, throws_when_push_an_element_in_full_stack) {
 	TStack<int> st(3);
+
+
 	for (int i = 0; i < 3; i++) {
 		st.push(i);
 	}
+
+
 	ASSERT_ANY_THROW(st.push(3));
 }
 
 TEST(TStack, can_assign_stack_to_itself)
 {
 	TStack<int> st(3);
+
 	ASSERT_NO_THROW(st = st);
 }
 
 TEST(TStack, can_assign_stacks_of_equal_maxsize)
 {
 	TStack<int> a(3), b(3);
+
 	b = a;
+
 	EXPECT_EQ(a, b);
 }
 
 TEST(TStack, assign_operator_changes_stack_maxsize)
 {
 	TStack<int> a(3), b(5);
+
 	b = a;
+
 	EXPECT_EQ(3, b.getmaxsize());
 }
